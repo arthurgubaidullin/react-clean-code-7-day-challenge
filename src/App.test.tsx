2a4b4ee1bcdc0e698cd83b3eba3ebc1f0d9c3632
Artur Gubaidullin <lapteuh@gmail.com>
2023-03-30 21:05:41 +0400

    Add test for submit button

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,3 +7,10 @@ test("renders learn react link", () => {
   const linkElement = screen.getByText("My Todo Application");
   expect(linkElement).toBeInTheDocument();
 });
+
+test("renders submit button", () => {
+  render(<App />);
+  const submitButton: HTMLButtonElement = screen.getByRole("button");
+  expect(submitButton).toBeInTheDocument();
+  expect(submitButton?.textContent).toEqual("Submit");
+});
