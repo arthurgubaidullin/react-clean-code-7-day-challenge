1a82029931c09c6b4577a3c11a68be047c60e95e
Artur Gubaidullin <lapteuh@gmail.com>
2023-03-30 21:06:06 +0400

    Add "Submit" button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div>My Todo Application</div>;
+  return (
+    <div>
+      My Todo Application
+      <button>Submit</button>
+    </div>
+  );
 }
 
 export default App;
